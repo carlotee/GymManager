ee2d1b14313ba9fa852f8e9cb21ceb75e13b99d0
Carlos Vega <carlosvega3031@gmail.com>
2026-08-17 16:24:42 -0400

    front end del login configurado+rutas

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html'
 })
-export class Login {
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  login() {
+    console.log('Correo:', this.email);
+    console.log('Contraseña:', this.password);
+
+    if (this.email === 'admin@gmail.com' && this.password === '123456') {
+      console.log('Login correcto');
+    } else {
+      console.log('Correo o contraseña incorrectos');
+    }
+  }
+  irAlRegistro() {
+    console.log('Redirigiendo al registro');
+  }
 
 }
